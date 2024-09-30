0f15ff9d5e88e1972348a7dd9ac5366f48c36d72
Timothy <tcraig258@gmail.com>
2024-09-30 11:32:24 +0200

    deleted tdcontroller for proper install

--- submodules/tdcontroller/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='tdtomonapari',  # Replace with your package name
-    version='0.0.1',  # Replace with your package version
-    packages=find_packages(),
-    install_requires=[
-    ],
-)
